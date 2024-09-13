dd5edf1ce80896f2dbce837ff0d78d4dc89ffe9d
Babak Karimi Bavandpour <babak.bavandpour@livecricketdata.com>
2024-09-13 12:36:14 +0330

    Add OS check

--- build.rs
+++ build.rs
@@ -3,8 +3,11 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable is not set");
-    let src_path = "SDL2.dll";
-    let dest_path = Path::new(&out_dir).join("SDL2.dll");
-    fs::copy(src_path, dest_path).expect("Failed to copy file");
+    #[cfg(target_os = "windows")]
+    {
+        let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable is not set");
+        let src_path = "SDL2.dll";
+        let dest_path = Path::new(&out_dir).join("SDL2.dll");
+        fs::copy(src_path, dest_path).expect("Failed to copy file");
+    }
 }
