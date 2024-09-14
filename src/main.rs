b7066127f6ac7d53ad9966f1cb2dbcbff9e7ce2d
Babak Karimi Bavandpour <babak.bavandpour@livecricketdata.com>
2024-09-14 13:10:06 +0330

    Add command-line options

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,75 @@ mod events;
 mod helpers;
 
 use std::time::{Duration, Instant};
-// use async_std::print;
+use async_std::print;
 use async_std::task;
 use helpers::model_helper;
-// use helpers::test_helper;
+use regex::Regex;
 use std::env;
 
 use types::{view_state::ViewState, light::Light, event_callback::EventCallback, renderer::Renderer};
 use graphics::{gpu::gpu_renderer::GpuRenderer, cpu::cpu_renderer::CpuRenderer};
 use events::event_handler::EventHandler;
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+const DEFAULT_WIDTH: u32 = 800;
+const DEFAULT_HEIGHT: u32 = 600;
 
 const FPS: u32 = 60;
 const FRAME_DURATION: Duration = Duration::from_millis(1000 / FPS as u64);
 
 #[async_std::main]
 async fn main() {
+    let mut gpu_enabled = true;
+    let mut framerate_log = false;
+    let mut fullscreen = false;
+    let mut width: Option<u32> = None;
+    let mut height: Option<u32> = None;
+
+    let width_regex = Regex::new(r"^w=(\d+)$").unwrap();
+    let height_regex = Regex::new(r"^h=(\d+)$").unwrap();
+
     let args: Vec<String> = env::args().collect();
-    let gpu_enabled = if let Some(arg) = args.get(1) {
-        match arg.as_str() {
-            "gpu" => true,
-            "cpu" => false,
-            _ => panic!("Invalid argument: {}. Please use 'gpu' or 'cpu'.", arg),
+    for i in 1..args.len() {
+        if let Some(arg) = args.get(i) {
+            match arg.as_str() {
+                "gpu" => gpu_enabled = true,
+                "cpu" => gpu_enabled = false,
+                "framerate" => framerate_log = true,
+                "fullscreen" => fullscreen = true,
+                _ if width_regex.is_match(arg) => {
+                    if let Some(caps) = width_regex.captures(arg) {
+                        width = caps.get(1).map(|m| m.as_str().parse().unwrap());
+                    }
+                }
+                _ if height_regex.is_match(arg) => {
+                    if let Some(caps) = height_regex.captures(arg) {
+                        height = caps.get(1).map(|m| m.as_str().parse().unwrap());
+                    }
+                }
+                _ => panic!("Invalid argument: {}. Please use 'gpu', 'cpu', 'framerate', 'fullscreen', 'w={{}}', or 'h={{}}'.", arg),
+            }
         }
-    } else {
-        true
-    };
+    }
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let display_mode = video_subsystem.desktop_display_mode(0).unwrap();
 
-    let window = video_subsystem.window("Perfect Engine", WIDTH, HEIGHT)
+    let width = if let Some(w) = width {w} else { if fullscreen { display_mode.w as u32 } else {DEFAULT_WIDTH} };
+    let height = if let Some(h) = height {h} else { if fullscreen { display_mode.h as u32 } else {DEFAULT_HEIGHT} };
+
+    let window = if fullscreen {
+        video_subsystem.window("Perfect Engine", width, height)
+        .fullscreen()
+        .build()
+        .unwrap()
+    } else {
+        video_subsystem.window("Perfect Engine", width, height)
         .position_centered()
         .resizable()
-        .vulkan()
         .build()
-        .unwrap();
+        .unwrap()
+    };
 
     let texture_creator;
     let mut renderer: Box<dyn Renderer<'static>> = if gpu_enabled {
@@ -52,13 +83,8 @@ async fn main() {
         Box::new(CpuRenderer::new(canvas, &texture_creator))
     };
 
-    let mut total_pixel_count = 0;
-    // let (pixels, pixel_count) = test_helper::generate_cube_pixels(400000, 4.0);
-    // renderer.load_pixels(pixels);
-    // total_pixel_count += pixel_count;
     let (pixels, pixel_count) = model_helper::load_msh_file_with_texture().await;
     renderer.load_pixels(pixels);
-    total_pixel_count += pixel_count;
 
     let event_pump = sdl_context.event_pump().unwrap();
     let mut event_handler = EventHandler::new(event_pump);
@@ -76,7 +102,8 @@ async fn main() {
         intensity: 40.0,
     };
 
-    println!("\nGPU ENABLED:  {}\t\tFPS LIMIT: {:5}\tPIXEL COUNT: {:10}", gpu_enabled, FPS, total_pixel_count);
+    println!("\nFULLSCREEN:   {}\t\tWIDTH: {}\t\tHEIGHT: {}", fullscreen, width, height);
+    println!("GPU ENABLED:  {}\t\tFPS LIMIT: {:5}\tPIXEL COUNT: {:10}", gpu_enabled, FPS, pixel_count);
     'running: loop {
         let process_start = Instant::now();
 
@@ -88,7 +115,9 @@ async fn main() {
         }
 
         let process_duration = process_start.elapsed();
-        // print!("\rFRAME TIME: {:4}ms\t\tFRAME RATE: {:4}", process_duration.as_millis(), 1000 / process_duration.as_millis()).await;
+        if framerate_log {
+            print!("\rFRAME TIME: {:4}ms\t\tFRAME RATE: {:4}", process_duration.as_millis(), 1000 / process_duration.as_millis()).await;
+        }
         if process_duration < FRAME_DURATION {
             task::sleep(FRAME_DURATION - process_duration).await;
         }
